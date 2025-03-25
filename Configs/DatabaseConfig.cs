5620c6e6e971e2584ddaf024b75d1451b83729e7
helloMinh <phandinhminh48@gmail.com>
2025-03-25 21:14:17 +0700

    b

--- Configs/DatabaseConfig.cs
+++ Configs/DatabaseConfig.cs
@@ -23,7 +23,7 @@ public class DatabaseConfig
             Directory.CreateDirectory(appDataPath);
         }
 
-        return Path.Combine(appDataPath, "quanlydaily.db");
+        return Path.Combine(appDataPath, "quanlydaily1.db");
     }
 
     public async Task Initialize(string? dbPath = null)
