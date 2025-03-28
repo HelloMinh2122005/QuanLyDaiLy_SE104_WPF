cdf169eecc3272180cb898de5374c3da966273c2
helloMinh <phandinhminh48@gmail.com>
2025-03-28 12:11:46 +0700

    added required

--- Models/LoaiDaiLy.cs
+++ Models/LoaiDaiLy.cs
@@ -7,6 +7,9 @@ namespace QuanLyDaiLy.Models
     {
         [Key]
         public int MaLoaiDaiLy { get; set; } = 0;
+
+        [Required(ErrorMessage = "Tên loại đại lý không được để trống")]
+        [StringLength(100, ErrorMessage = "Tên loại đại lý không được vượt quá 100 ký tự")]
         public string TenLoaiDaiLy { get; set; } = "";
         public int NoToiDa { get; set; } = 0;
 
