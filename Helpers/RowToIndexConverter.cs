ec5cd75e80c902595968d9f9bb96a84b951d8241
helloMinh <phandinhminh48@gmail.com>
2025-04-02 14:48:56 +0700

    fixed pages

---
+++ Helpers/RowToIndexConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace QuanLyDaiLy.Helpers
+{
+    public class RowToIndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int index)
+            {
+                return (index + 1).ToString();
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
