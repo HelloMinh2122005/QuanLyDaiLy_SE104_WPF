90e00a50d99490f468c9edca09f46930ee5e75ab
phamtranxuan-tien <23521583@gm.uit.edu.vn>
2025-05-16 11:13:17 +0700

    update StringHelper

--- Helpers/StringHelper.cs
+++ Helpers/StringHelper.cs
@@ -5,11 +5,6 @@ namespace QuanLyDaiLy.Helpers
 {
     public static class StringHelper
     {
-        /// <summary>
-        /// Loại bỏ dấu (dấu tiếng Việt) khỏi chuỗi đầu vào.
-        /// </summary>
-        /// <param name="text">Chuỗi có dấu</param>
-        /// <returns>Chuỗi đã loại bỏ dấu</returns>
         public static string RemoveDiacritics(string text)
         {
             if (string.IsNullOrWhiteSpace(text))
